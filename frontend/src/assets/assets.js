dc903a92026fcb7f7d821a86e1dbf166bbc1c0f5
muhammedgulten <muhammedgultenn@gmail.com>
2025-05-05 16:58:21 +0300

    first commit

---
+++ frontend/src/assets/assets.js
@@ -0,0 +1,229 @@
+import basket_icon from './basket_icon.png'
+import logo from './logo.png'
+import header_img from './header_img.png'
+import search_icon from './search_icon.png'
+import menu_1 from './menu_1.png'
+import menu_2 from './menu_2.png'
+import menu_3 from './menu_3.png'
+import menu_4 from './menu_4.png'
+import menu_5 from './menu_5.png'
+
+import food_1 from './food_1.png'
+import food_2 from './food_2.png'
+import food_3 from './food_3.png'
+import food_4 from './food_4.png'
+import food_5 from './food_5.png'
+import food_6 from './food_6.png'
+import food_7 from './food_7.png'
+import food_8 from './food_8.png'
+import food_9 from './food_9.png'
+import food_10 from './food_10.png'
+import food_11 from './food_11.png'
+import food_12 from './food_12.png'
+import food_13 from './food_13.png'
+import food_14 from './food_14.png'
+import food_15 from './food_15.png'
+import food_16 from './food_16.png'
+import food_17 from './food_17.png'
+import food_18 from './food_18.png'
+import food_19 from './food_19.png'
+
+
+
+import add_icon_white from './add_icon_white.png'
+import add_icon_green from './add_icon_green.png'
+import remove_icon_red from './remove_icon_red.png'
+import app_store from './app_store.png'
+import play_store from './play_store.png'
+import linkedin_icon from './linkedin_icon.png'
+import facebook_icon from './facebook_icon.png'
+import twitter_icon from './twitter_icon.png'
+import cross_icon from './cross_icon.png'
+import selector_icon from './selector_icon.png'
+import rating_starts from './rating_starts.png'
+import profile_icon from './profile_icon.png'
+import bag_icon from './bag_icon.png'
+import logout_icon from './logout_icon.png'
+import parcel_icon from './parcel_icon.png'
+
+export const assets = {
+    logo,
+    basket_icon,
+    header_img,
+    search_icon,
+    rating_starts,
+    add_icon_green,
+    add_icon_white,
+    remove_icon_red,
+    app_store,
+    play_store,
+    linkedin_icon,
+    facebook_icon,
+    twitter_icon,
+    cross_icon,
+    selector_icon,
+    profile_icon,
+    logout_icon,
+    bag_icon,
+    parcel_icon
+}
+
+export const menu_list = [
+    {
+        menu_name: "Burgers",
+        menu_image: menu_1
+    },
+    {
+        menu_name: "Salads",
+        menu_image: menu_2
+    },
+    {
+        menu_name: "Snacks",
+        menu_image: menu_3
+    },
+    {
+        menu_name: "Sauces",
+        menu_image: menu_4
+    },
+    {
+        menu_name: "Drinks",
+        menu_image: menu_5
+    }]
+
+export const food_list = [
+    {
+        _id: "1",
+        name: "Burger",
+        image: food_1,
+        price: 12,
+        description: "Food provides essential nutrients for overall health and well-being",
+        category: "Burgers"
+    },
+    {
+        _id: "2",
+        name: "Double Burger",
+        image: food_2,
+        price: 18,
+        description: "Food provides essential nutrients for overall health and well-being",
+        category: "Burgers"
+    }, {
+        _id: "3",
+        name: "Steakhouse",
+        image: food_3,
+        price: 16,
+        description: "Food provides essential nutrients for overall health and well-being",
+        category: "Burgers"
+    }, {
+        _id: "4",
+        name: "Double Steakhouse",
+        image: food_4,
+        price: 24,
+        description: "Food provides essential nutrients for overall health and well-being",
+        category: "Burgers"
+    }, {
+        _id: "5",
+        name: "Cheeseburger",
+        image: food_5,
+        price: 14,
+        description: "Food provides essential nutrients for overall health and well-being",
+        category: "Burgers"
+    }, {
+        _id: "6",
+        name: "Chicken Salad",
+        image: food_6,
+        price: 20,
+        description: "Food provides essential nutrients for overall health and well-being",
+        category: "Salads"
+    }, {
+        _id: "7",
+        name: "Fresh Salad",
+        image: food_7,
+        price: 15,
+        description: "Food provides essential nutrients for overall health and well-being",
+        category: "Salads"
+    }, {
+        _id: "8",
+        name: "Fries",
+        image: food_8,
+        price: 15,
+        description: "Food provides essential nutrients for overall health and well-being",
+        category: "Snacks"
+    }, {
+        _id: "9",
+        name: "Nuggets",
+        image: food_9,
+        price: 15,
+        description: "Food provides essential nutrients for overall health and well-being",
+        category: "Snacks"
+    }, {
+        _id: "10",
+        name: "Cheese Sticks",
+        image: food_10,
+        price: 15,
+        description: "Food provides essential nutrients for overall health and well-being",
+        category: "Snacks"
+    }, {
+        _id: "11",
+        name: "Ketchup",
+        image: food_11,
+        price: 15,
+        description: "Food provides essential nutrients for overall health and well-being",
+        category: "Sauces"
+    }, {
+        _id: "12",
+        name: "Mayonnaise",
+        image: food_12,
+        price: 15,
+        description: "Food provides essential nutrients for overall health and well-being",
+        category: "Sauces"
+    }, {
+        _id: "13",
+        name: "Garlic",
+        image: food_13,
+        price: 15,
+        description: "Food provides essential nutrients for overall health and well-being",
+        category: "Sauces"
+    }, {
+        _id: "14",
+        name: "BBQ",
+        image: food_14,
+        price: 15,
+        description: "Food provides essential nutrients for overall health and well-being",
+        category: "Sauces"
+    }, {
+        _id: "15",
+        name: "Coca-Cola",
+        image: food_15,
+        price: 15,
+        description: "Food provides essential nutrients for overall health and well-being",
+        category: "Drinks"
+    }, {
+        _id: "16",
+        name: "Fanta",
+        image: food_16,
+        price: 15,
+        description: "Food provides essential nutrients for overall health and well-being",
+        category: "Drinks"
+    }, {
+        _id: "17",
+        name: "Sprite",
+        image: food_17,
+        price: 15,
+        description: "Food provides essential nutrients for overall health and well-being",
+        category: "Drinks"
+    }, {
+        _id: "18",
+        name: "Beer",
+        image: food_18,
+        price: 15,
+        description: "Food provides essential nutrients for overall health and well-being",
+        category: "Drinks"
+    }, {
+        _id: "19",
+        name: "Orange Drink",
+        image: food_19,
+        price: 15,
+        description: "Food provides essential nutrients for overall health and well-being",
+        category: "Drinks"
+    }
+]
